7d57c3c2008bfc47bfad752fbac09fbd0be5c597
THEODORO MARTINS DE FREITAS <theomartins137@gmail.com>
2025-01-20 20:03:34 -0300

    Atualização do exercício

--- Ex011/Program.cs
+++ Ex011/Program.cs
@@ -4,12 +4,25 @@
     {
         static void Main(string[] args)
         {
+         
+
             Console.Write("Qual o primeiro lado do triângulo?");
             int n1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Qual o segundo lado do triângulo?");
             int n2 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Qual o terceiro lado do triângulo?");
             int n3 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 < n2 + n3 && n2 < n1 + n3 && n3 < n2 + n1)
+            {
+                Console.WriteLine("Seu triângulo é válido");
+            }
+
+            else
+            {
+                Console.WriteLine("Seu triângulo é inválido");
+                return;
+            }            
 
             if (n1 == n2 && n2 == n3)
             {
