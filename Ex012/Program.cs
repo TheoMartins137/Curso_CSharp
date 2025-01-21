b662b18d3d98c5cebd8f0bc8eb93e4019c8f927c
THEODORO MARTINS DE FREITAS <theomartins137@gmail.com>
2025-01-20 22:00:20 -0300

    Calculo de aumento

---
+++ Ex012/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ex012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1;
+
+            Console.Write("Qual o salário do funcionario? ");
+            double salario = Convert.ToInt32(Console.ReadLine());
+
+            if (salario > 2500)
+            {
+                num1 = salario * 0.05;
+                Console.WriteLine("O salario é de: R${0},o ajuste é de: R${1} após os ajustes ficou: {2} ", salario, num1, salario+num1);
+            }
+            else if (salario > 1250)
+            {
+                num1 = salario * 0.10;
+                Console.WriteLine("O salario é de: R${0},o ajuste é de: R${1} após os ajustes ficou: {2} ", salario, num1, salario + num1);
+            }
+            else
+            {
+                num1 = salario * 0.15;
+                Console.WriteLine("O salario é de: R${0},o ajuste é de: R${1} após os ajustes ficou: {2} ", salario, num1, salario + num1);
+            }
+        }
+    }
+}
