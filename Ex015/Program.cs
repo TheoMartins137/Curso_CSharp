c8fc94c1c8fe4b2cefad23e48d356e12d73e1f06
THEODORO MARTINS DE FREITAS <theomartins137@gmail.com>
2025-01-22 22:09:19 -0300

    atualização

---
+++ Ex015/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string genero;
+            Console.Write("Digite o gênero: ");
+            genero = Console.ReadLine().ToUpper();
+
+            while (genero != "F" && genero != "M")
+            {
+                Console.Write("Digite o gênero: ");
+                genero= Console.ReadLine().ToUpper();
+            }
+
+            Console.WriteLine("Bem-vindo e bem-vindas ao curso de C#");
+        }
+    }
+}
