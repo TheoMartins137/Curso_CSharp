677a91ed2e73379f9b346de3c4161caac562dd21
THEODORO MARTINS DE FREITAS <theomartins137@gmail.com>
2025-01-22 21:01:44 -0300

    Aula 22/01/25

---
+++ Ex013/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+            Console.WriteLine("Todos os pares de um intervalo");
+            Console.WriteLine("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+            Console.WriteLine();    
+            Console.Write("Digite o final do intervalo: ");
+            int final = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= final; i++)
+
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ teste_for/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace teste_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0; i <= 50; i+= 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
