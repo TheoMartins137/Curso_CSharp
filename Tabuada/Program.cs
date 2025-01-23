310c68cb5779a5c4b85201e2f399149f6a64b73a
THEODORO MARTINS DE FREITAS <theomartins137@gmail.com>
2025-01-22 21:34:52 -0300

    Novo exercício

--- Tabuada/Program.cs
+++ Tabuada/Program.cs
@@ -4,17 +4,12 @@
     {
         static void Main(string[] args)
         {
-            int num2 = 1;
-
-            Console.WriteLine("Escreva um número: ");
+            Console.Write("Escreva um número: ");
             int num1 = Convert.ToInt32(Console.ReadLine());
 
-            while (num2 < 11)
+            for (int i = 1; i <= 10; i++)
             {
-                Console.WriteLine("{0} vezes {1} = {2}", num1, num2, num1*num2);
-                num2++;
-
-
+                Console.WriteLine("{0} X {1} = {2} ", num1,i, num1 * i);
             }
         }
     }
