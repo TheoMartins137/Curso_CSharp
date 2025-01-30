bcef046da89b10d1c75686e4b6eef7a200a5926d
THEODORO MARTINS DE FREITAS <theomartins137@gmail.com>
2025-01-29 22:15:30 -0300

    atualização

---
+++ Area_Triangulo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace Area_Triangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========================");
+            Console.WriteLine("Cálcular a Área do Triângulo");
+            Console.WriteLine("==========================");
+            Console.WriteLine();
+
+            Console.WriteLine("Escreva qual a base do triângulo: ");
+            int bas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escreva qual a altura do triângulo: ");
+            int altura = Convert.ToInt32(Console.ReadLine());
+
+            area(bas, altura);
+        }
+
+        static void area(int b, int al)
+        {
+            double a = (b * al) / 2;
+            Console.WriteLine($"Um triângulo com as dimensões de base = {b} e altura= {al}, tem uma área de {a:F2}m²");
+        }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 1;
+
+            while (cont == 1)
+            {
+            Console.WriteLine("===============");
+            Console.WriteLine("--CAlCULADORA--");
+            Console.WriteLine("===============");
+            Console.WriteLine();
+
+            Console.WriteLine("Escreva o primeiro número: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escreva o segundo número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+
+            Console.WriteLine("===================================");
+            Console.WriteLine("Qual operação você deseja fazer?");
+            Console.WriteLine("- [1] ADIÇÂO ");
+            Console.WriteLine("- [2] SUBTRAÇÂO ");
+            Console.WriteLine("- [3] MULTIPLICAÇÂO ");
+            Console.WriteLine("- [4] DIVISÂO ");
+            int resposta = Convert.ToInt32(Console.ReadLine());
+
+            if (resposta == 1)
+            {
+                soma(num1, num2);
+            }
+
+            else if (resposta == 2)
+            {
+                sub(num1, num2);
+            }
+
+            else if (resposta == 3)
+            {
+                mult(num1, num2);
+            }
+
+            else
+            {
+                div(num1, num2);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Deseja refazer? ");
+            Console.WriteLine("-[1] SIM");
+            Console.WriteLine("-[2] NÃO");
+            cont = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            }
+
+
+        }
+
+        static void soma(int soma, int somb)
+        {
+            int resultado = soma + somb;
+            Console.WriteLine($"{soma} + {somb} = {resultado}");
+            Console.WriteLine();
+        }
+
+        static void sub(int soma, int somb)
+        {
+            int resultado = soma - somb;
+            Console.WriteLine($"{soma} - {somb} = {resultado}");
+            Console.WriteLine();
+        }
+
+        static void mult(int soma, int somb)
+        {
+            int resultado = soma * somb;
+            Console.WriteLine($"{soma} * {somb} = {resultado}");
+            Console.WriteLine();
+        }
+
+        static void div(int soma, int somb)
+        {
+            int resultado = soma / somb;
+            Console.WriteLine($"{soma} / {somb} = {resultado}");
+            Console.WriteLine();
+        }
+    }
+}
