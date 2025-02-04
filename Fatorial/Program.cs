b519cff5d391daaaa233c9d7a1b8f1a23c47b3ad
THEODORO MARTINS DE FREITAS <theomartins137@gmail.com>
2025-02-03 21:17:31 -0300

    03/02/25

---
+++ Fatorial/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Fatorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            char resp;
+            bool mostrar = false;
+
+            Console.Write("Digite um número inteiro: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Deseja mostrar os cálculos? (S/N)");
+            resp =  Convert.ToChar(Console.ReadLine().ToUpper());
+
+            if (resp == 'S') { mostrar = true; }
+
+            Console.WriteLine(fatorial(num, mostrar));
+         
+        }
+
+        static int fatorial(int n, bool show =  false)
+        {
+            int f = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (show)
+                {
+                    Console.Write(i);
+                    if (i < n)
+                    {
+                        Console.Write(" X ");
+                    }
+                    else
+                    {
+                        Console.Write(" = ");
+                    }
+                }
+
+                f *= i;
+            }
+            return f;
+        }
+    }
+}
---
+++ Votacao/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Votacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nasce;
+            Console.Write("Digite o ano do seu nascimento: ");
+            nasce = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(voto(nasce));
+        }
+
+        static string voto (int ano)
+        {
+            DateTime dataAtual = DateTime.Now;
+            int anoAtual = dataAtual.Year;
+            int idade = anoAtual - ano;
+
+            if (idade < 16)
+            {
+                return $"Com {idade} anos, você não vota!";
+            }
+            else if (idade > 18 && idade < 70)
+            {
+                return $"Com {idade} anos, seu voto é OBRIGATÓRIO!";
+            }
+            else
+            {
+                return $"Com {idade} anos, seu voto é OPCIONAL!";
+
+            }
+        }
+    }
+}
